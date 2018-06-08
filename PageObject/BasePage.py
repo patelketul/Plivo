73cf01a6f901e5a70b2b3c7219da8d96fb480197
patelketul <ketulyash@gmail.com>
2018-06-09 01:37:51 +0530

    Update BasePage.py

--- PageObject/BasePage.py
+++ PageObject/BasePage.py
@@ -4,43 +4,23 @@ import selenium.webdriver.support.expected_conditions as EC
 class BasePage():
 
     #wait for element to render on screen
-    #Phantomjs has some issues with visibility_of_element_located. Added try-except statement to handle the issue
     def waitForElement(self, driver,locator,wait=30):
-        try:
-            return WebDriverWait(driver, wait).until(EC.visibility_of_element_located(locator))
-        except:
-            return WebDriverWait(driver, wait).until(EC.presence_of_element_located(locator))
-
-
-    def waitForAllElements(self,driver,locator,wait=30):
-        try:
-            return WebDriverWait(driver,wait).until(EC.visibility_of_all_elements_located(locator))
-        except:
-            return WebDriverWait(driver, wait).until(EC.presence_of_all_elements_located(locator))
+        return WebDriverWait(driver, wait).until(EC.visibility_of_element_located(locator))
 
     #Get element
     def getElement(self, driver,locator,wait=30):
-        try:
-            return WebDriverWait(driver, wait).until(EC.visibility_of_element_located(locator))
-        except:
-            return WebDriverWait(driver, wait).until(EC.presence_of_element_located(locator))
-
+        return WebDriverWait(driver, wait).until(EC.visibility_of_element_located(locator))
 
     def waitForElementToBeClickable(self,driver,locator,wait=30):
         WebDriverWait(driver,wait).until(EC.element_to_be_clickable(locator))
 
-    def waitForElementToBeInvisible(self, driver, locator, wait=30):
-        WebDriverWait(driver, wait).until(EC.invisibility_of_element_located(locator))
-
     #Wait for element to render and click
     def waitAndClick(self,driver,locator):
-        self.waitForElement(driver,locator)
         self.waitForElementToBeClickable(driver,locator)
         self.getElement(driver,locator).click()
 
     #Wait for element to render and type text
     def waitAndType(self,driver,text,locator):
-        self.waitForElement(driver,locator)
         self.waitForElementToBeClickable(driver,locator)
         self.getElement(driver,locator).send_keys(text)
 
@@ -54,13 +34,9 @@ class BasePage():
         except:
             return False
 
-    #Check if element is displayed or not
-    def isElementDisplayed(self,element):
-        return element.is_displayed()
-
     #Return text from element
     def getElementText(self,element):
-        return element.get_attribute('textContent')#element.text
+        return element.get_attribute('textContent')
 
 
 
